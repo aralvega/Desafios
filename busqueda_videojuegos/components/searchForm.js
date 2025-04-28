2ae8748089e0409dcd10917f76b9e8af5aecefa8
ArielAVega <avega@fi.unju.edu.ar>
2025-04-28 20:42:18 -0300

    feat: Creación de los módulos de los componentes y ejecución de los eventos [Ariel Vega] [Busqueda de videojuegos]

---
+++ busqueda_videojuegos/components/searchForm.js
@@ -0,0 +1,63 @@
+import { buscarPorId,buscarPorNombre,buscarPorFecha } from "../services/videojuegoService.js";
+import { inicializarTabla } from "./videojuegoTabla.js";
+
+export const inicializarBuscador = (data)=>{
+    const container = document.getElementById('search-form');
+    container.innerHTML = `
+        <div>
+            <label><input type='radio' name='filtro' value='id'>ID</label>
+            <label><input type='radio' name='filtro' value='nombre'>Nombre</label>
+            <label><input type='radio' name='filtro' value='fecha'>Fecha</label>
+            <input type='text' id='inputValor' placeholder='Ingrese valor' disabled>
+            <button id='btnBuscar'>Buscar</button>
+            <label><input type='checkbox' id='chkLimpiar'>Limpiar</label>
+        </div>
+    `;
+
+    const radios = container.querySelectorAll('input[name="filtro"]');
+    const inputValor = container.querySelector('#inputValor');
+    const btnBuscar = container.querySelector('#btnBuscar');
+    const chkLimpiar = container.querySelector('#chkLimpiar');
+
+    radios.forEach(radio =>{
+        radio.addEventListener('change',()=>{
+            inputValor.disabled = false;
+            switch(radio.value){
+                case 'id':inputValor.type = 'number';break;
+                case 'nombre':inputValor.type = 'text';break;
+                case 'fecha':inputValor.type = 'date';break;
+            }
+            inputValor.value = '';
+        });
+    });
+
+    btnBuscar.addEventListener('click',()=>{
+        const seleccionado = container.querySelector('input[name="filtro"]:checked');
+        if(!seleccionado) 
+            return;
+        const valor = inputValor.value;
+        let resultados  =data;
+        switch(seleccionado.value){
+            case 'id': 
+                resultados = buscarPorId(data,parseInt(valor));
+                break;
+            case 'nombre': 
+                resultados = buscarPorNombre(data,valor);
+                break;
+            case 'fecha': 
+                resultados = buscarPorFecha(data,valor);
+                break;
+        }
+        inicializarTabla('videogames-table',resultados);
+    });
+
+    chkLimpiar.addEventListener('change',()=>{
+        if(chkLimpiar.checked){
+            inicializarTabla('videogames-table',data);
+            chkLimpiar.checked = false;
+            inputValor.value = '';
+            radios.forEach(r=>r.checked = false);
+            inputValor.disabled = true;
+        }
+    });
+};
--- busqueda_videojuegos/main.js
+++ busqueda_videojuegos/main.js
@@ -1,5 +1,13 @@
-import {videojuegos,buscarPorId,buscarPorNombre,buscarPorFecha } from "./services/videojuegoService.js";
-
+//import {videojuegos,buscarPorId,buscarPorNombre,buscarPorFecha } from "./services/videojuegoService.js";
 //console.log(buscarPorId(videojuegos,2));
 //console.log(buscarPorNombre(videojuegos,'Mario'));
-console.log(buscarPorFecha(videojuegos,'1990-11-21'));
+//console.log(buscarPorFecha(videojuegos,'1990-11-21'));
+
+import { videojuegos } from "./services/videojuegoService.js";
+import { inicializarTabla } from "./components/videojuegoTabla.js";
+import { inicializarBuscador } from "./components/searchForm.js";
+
+document.addEventListener('DOMContentLoaded',()=>{
+    inicializarTabla('videogames-table',videojuegos);
+    inicializarBuscador(videojuegos);
+});
